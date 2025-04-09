9aa5fb4be6245dac62f6111d25533fd01d8b41fa
Kavya Sharma <88178186+deviill007@users.noreply.github.com>
2025-04-09 23:06:54 +0530

    Error fixed

---
+++ src/pages/jaipur/experience/nature/elephantWalks.tsx
@@ -0,0 +1,11 @@
+const ElephantWalks = () => {
+    return (
+      <div>
+        <h1>Elephant Walks Experience</h1>
+        {/* Your content */}
+      </div>
+    );
+  };
+  
+  export default ElephantWalks;
+  
---
+++ src/pages/jaipur/experience/nature/leopardSafari.tsx
@@ -0,0 +1,11 @@
+const LeopardSafari = () => {
+    return (
+      <div>
+        <h1>Leopard Safari Experience</h1>
+        {/* Your content */}
+      </div>
+    );
+  };
+  
+  export default LeopardSafari;
+  
